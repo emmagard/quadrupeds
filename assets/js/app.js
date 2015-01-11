cb78910dfdb9445fddfe35914cb53b8d4c5e045b
Emma Gardner <emmagardner@outlook.com>
2015-01-11 15:24:47 -0500

    cleaned out some images I won't be using

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,25 @@
 $(document).foundation();
-$(document).ready(function(){
 
 
-});
+// when you click on the anchor tags in the about us section:
+var staff = document.getElementById('staff');
+var serviceArea = document.getElementById('service-area');
+var faqs = document.getElementById('faqs');
+
+var showText = function(){
+  console.log("show text");
+  var icon = this.children[0];
+  var hiddenText = this.parentElement.nextElementSibling;
+
+  // toggle hidden text
+  hiddenText.classList.toggle("visible");
+  // toggle chevron icon
+  icon.classList.toggle("fa-chevron-down");
+
+};
+
+staff.onclick = showText;
+serviceArea.onclick = showText;
+faqs.onclick = showText;
+
+
