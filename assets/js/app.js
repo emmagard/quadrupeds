ec9ef6fd4be5577db2b47bb1bab794e449ece858
Emma Gardner <emmagardner@outlook.com>
2015-01-12 22:11:32 -0500

    added staff section and made H1s drop downs

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,24 +2,25 @@ $(document).foundation();
 
 
 // when you click on the anchor tags in the about us section:
-var staff = document.getElementById('staff');
-var serviceArea = document.getElementById('service-area');
 var faqs = document.getElementById('faqs');
+var about = document.getElementById('about');
+var servicesAndRates  = document.getElementById('services-and-rates');
+var contact = document.getElementById('contact');
 
 var showText = function(){
   console.log("show text");
-  var icon = this.children[0];
+  //var icon = this.children[0];
   var hiddenText = this.parentElement.nextElementSibling;
 
   // toggle hidden text
   hiddenText.classList.toggle("visible");
   // toggle chevron icon
-  icon.classList.toggle("fa-chevron-down");
+  //icon.classList.toggle("fa-chevron-down");
 
 };
 
-staff.onclick = showText;
-serviceArea.onclick = showText;
-faqs.onclick = showText;
-
+//faqs.onclick = showText;
+about.onclick = showText;
+servicesAndRates.onclick = showText;
+contact.onclick = showText;
 
