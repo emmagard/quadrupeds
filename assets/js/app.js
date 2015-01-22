00104c3c479d4e64b4b0365cf418022da9516cb2
Emma Gardner <emmagardner@outlook.com>
2015-01-21 22:07:15 -0500

    oops I made too many changes! Added a media query for more responsiveness

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,10 @@ $(document).foundation();
 
 
 // when you click on the anchor tags in the about us section:
-var faqs = document.getElementById('faqs');
+
 var about = document.getElementById('about');
 var servicesAndRates  = document.getElementById('services-and-rates');
-var contact = document.getElementById('contact');
+
 
 var showText = function(){
   console.log("show text");
@@ -19,8 +19,8 @@ var showText = function(){
 
 };
 
-//faqs.onclick = showText;
+
 about.onclick = showText;
 servicesAndRates.onclick = showText;
-contact.onclick = showText;
+
 
